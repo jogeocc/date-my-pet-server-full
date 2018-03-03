efc286d9f5231aa6b16bde71279347ee09f6fbe2
--get-all <jogeochanc@gmail.com>
2018-03-03 13:32:30 -0600

    Anexo de lista de rutas

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
-//use Illuminate\Support\Facades\Route;
-use Illuminate\Routing\Route;
+use Illuminate\Support\Facades\Route;
+//use Illuminate\Routing\Route;
 /*
 |--------------------------------------------------------------------------
 | Web Routes

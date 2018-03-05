1de73361ddec62113a1d5f269acbdfe0f46f8370
--get-all <jogeochanc@gmail.com>
2018-03-05 02:09:53 -0600

    Agregando Metodo Actualizar Usuario

--- routes/api.php
+++ routes/api.php
@@ -48,7 +48,7 @@ Route::get('usuario/{idUsuario}/eliminarcuenta', [
     'as' => 'usuario.destroy'
 ]);
 
-Route::post('usuario/actualizar', [
+Route::put('usuario/{idUsuario}/actualizar', [
     'uses' => 'api\auth\UserController@update',
     'as' => 'usuario.update'
 ]);

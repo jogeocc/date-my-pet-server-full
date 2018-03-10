ae04446c874ca5c10db2b03772e78c122dfda08c
--get-all <jogeochanc@gmail.com>
2018-03-09 22:13:46 -0600

    Validando Formulario de editar user

--- app/Http/Controllers/api/auth/UserController.php
+++ app/Http/Controllers/api/auth/UserController.php
@@ -153,8 +153,40 @@ class UserController extends Controller
 
     public function update(Request $request, $idUsuario)
     {
-        //dd("Entro");
         $user = User::find($idUsuario);
+
+        $validator = Validator::make($request->all(), [
+            'username' => 'bail|required|max:30|unique:users,username,'.$user->id,
+            'correo' => 'bail|required|email|unique:users,correo|max:180',
+            'password' => 'bail|required|max:20',
+            'nombre'=>"required|max:180",
+            "direccion"=>"required",
+            "telefono"=>"nullable|max:20",
+            "celular"=>"required|max:20",
+        ],[
+            'username.required'=>"No ingresó su nombre de usuario",
+            'username.unique'=>"El nombre de usuario ya existe",
+            'username.max'=>"El nombre de usuario no debe excederse de los 30 caracteres",
+            'correo.unique'=>"El correo ya esta registrado",
+            'correo.required'=>"No ingresó el correo",
+            'correo.email'=>"El correo no tiene un formato valido",
+            'correo.max'=>"El correo no debe excederse de los 180 caracteres",
+            'password.required'=>"No ingresó la contraseña",
+            'password.max'=>"El password no debe excederde los 20 caracteres",
+            'nombre.required'=>"No ingresó su nombre completo",
+            'nombre.max'=>"Su nombre no debe excederse de los 180 caracteres",
+            "direccion.required"=>"No ingresó su dirección",
+            "telefono.max"=>"El número de teléfono no debe excederse de los 20 caracteres",
+            "celular.max"=>"El número celular no debe excederse de los 20 caracteres",
+            "celular.required"=>"No ingresó su número celular",
+        ]);
+
+
+        if ($validator->fails()) {
+            return response()->json(["errors"=>$validator->errors()], 401);            
+        }
+
+        
         $user->fill($request->all());
         $user->save();
         return response()->json(['success' => "$user->username, su cuenta se actualizó con éxito"], $this->successStatus);

684a357b894f75f726e23f40a11c90e826223784
--get-all <jogeochanc@gmail.com>
2018-03-19 11:52:45 -0600

    Verificando error de update mascota

--- app/Http/Controllers/MascotaController.php
+++ app/Http/Controllers/MascotaController.php
@@ -134,7 +134,7 @@ class MascotaController extends Controller
         $nombrefoto=null;
         $mascota=Mascota::find($idMascota);
 
-	dd($request->all());
+	dd($idMascota);
 
         $validator = Validator::make($request->all(), [
             'masNombre'=>'bail|required|max:100',

59e34c1deac285fe1af6eaaec447b908aac12cd7
--get-all <jogeochanc@gmail.com>
2018-03-24 11:28:31 -0600

    Agregando si tiene mascotas y veterinarios

--- app/Http/Controllers/VeterinarioController.php
+++ app/Http/Controllers/VeterinarioController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Veterinario;
+use App\User;
 use Validator;
 
 class VeterinarioController extends Controller

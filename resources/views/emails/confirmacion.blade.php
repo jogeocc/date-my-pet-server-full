53f44af206cf0674a1710199ad3e44c2e6cd0959
--get-all <jogeochanc@gmail.com>
2018-03-27 20:22:04 -0600

    Cambio aspecto del correo de confirmacion

--- resources/views/emails/confirmacion.blade.php
+++ resources/views/emails/confirmacion.blade.php
@@ -4,28 +4,64 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0">
     <title>Llamado de emergencia</title>
+    <link href="https://fonts.googleapis.com/css?family=Pacifico" rel="stylesheet">
     <style>
+        *{
+            margin:0;
+            font-size:15px;
+         }
+
         ul{
             list-style-image: url("http://date-my-pet-mx.tk/images/li-icon.png");
          }
 
+        .titular{
+            background:url("http://date-my-pet-mx.tk/images/titular.png");
+            padding:1em;
+            margin-bottom:0.5em;
+        }
+
+        .titular>*{
+            display:inline-block;
+            vertical-align: middle;
+        }
+
+        .titular>h1{
+            padding-left: 0.5em;
+            font-family: 'Pacifico', cursive;
+            font-weight: normal;
+            font-size: 2em;
+            color:#FFB74D;
+            
+         }
+
+        .titular>img{
+            width:5em;
+            transform: rotate(-20deg);
+         }
+
+        .contenido{
+            padding:0.5em;
+        }
+
     </style>
 </head>
 <body>
-    <p>Felicidades! Has creado tu cuenta en DateMyPet con los siguientes datos:</p>
-    <ul>
-        <li>Nombre de usuario: </li>
-        <li>Correo: </li>
-        <li>Nombre: </li>
-        <li>Teléfono: </li>
-        <li>Celular: </li>
-        <li>Dirección: </li>
-    </ul>
-    <p>Link de activación:</p>
-    <ul>
-        <a href="http://date-my-pet-mx.tk/">http://date-my-pet-mx.tk/</a>
-        
-        
-    </ul>
+    <div class="titular">
+        <img src="http://date-my-pet-mx.tk/images/huella1.png" />
+        <h1>Date My Pet</h1>
+    </div>
+    <div class="contenido">
+        <p>Felicidades! Has creado tu cuenta en DateMyPet con los siguientes datos:</p>
+        <ul>
+            <li>Nombre de usuario: </li>
+            <li>Correo: </li>
+            <li>Nombre: </li>
+            <li>Teléfono: </li>
+            <li>Celular: </li>
+            <li>Dirección: </li>
+        </ul>
+        <p>Link de activación: <a href="http://date-my-pet-mx.tk/">http://date-my-pet-mx.tk/</a></p>
+    </div>
 </body>
 </html>

8e0ca8147d3170c0b90194d4869066759a7fcd88
jogeocc <jogeochanc@gmail.com>
2018-03-28 15:21:33 -0600

    Cambio del msj de activacion

--- routes/web.php
+++ routes/web.php
@@ -34,26 +34,24 @@ foreach ($routeCollection as $value) {
 Route::any('activar/{token}', function($token) {
 	
       $user=User::where('remember_token','LIKE',"$token")->first();
-      
+      $activo =0;
       if($user!=null && count($user)>0){
             $user->activo=1;
             $user->save();
 
-        echo "Cuenta Activada <br>";
-        echo '<a href="intent:#Intent;action=com.example.jgchan.datemypet;category=android.intent.category.DEFAULT;category=android.intent.category.BROWSABLE;S.msg_from_browser=Launched%20from%20Browser;end">Presione Aqui para ir a la app</a>';
+        $activo=1;
 
-      }else{
-        echo "Error";
       }
+        
+      return view("activacion")->with(["activo"=>$activo]);
 
 });
 
 
 
 Route::any('Hora', function(){
-
       dd(\Carbon\Carbon::now()->format("d/m/Y h:i:s A"));
-
-      
 });
+
+
 

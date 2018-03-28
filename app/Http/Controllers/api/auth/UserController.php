1bd48d27e26dc2c12208b44f0fb3cebe2443a764
--get-all <jogeochanc@gmail.com>
2018-03-27 21:28:29 -0600

    Correccion del fallo en un . al concatenar el remember

--- app/Http/Controllers/api/auth/UserController.php
+++ app/Http/Controllers/api/auth/UserController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\User;
 use Auth;
+use Mail;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 use Laravel\Passport\Client;

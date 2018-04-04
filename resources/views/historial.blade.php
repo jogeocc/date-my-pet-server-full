ff409878eed1cd06786ef34e07e658e3b05f5afa
--get-all <jogeochanc@gmail.com>
2018-04-03 19:03:38 -0500

    Cambio en la ruta de visualizar hsitorial

--- resources/views/historial.blade.php
+++ resources/views/historial.blade.php
@@ -5,9 +5,8 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="icon" href="{{asset('images/huella2.png')}}" type="image/png">
-       <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+       <link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}">
+     
 
         <title>Date my pet | Historial Médico </title>
         
